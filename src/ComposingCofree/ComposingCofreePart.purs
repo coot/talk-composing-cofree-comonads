babb5d91557eb631af91543dd3f7b39ce1a93ffc
Marcin Szamotulski <mszamot@gmail.com>
2017-07-25 18:34:01 +0200

    indentation

--- src/ComposingCofree/ComposingCofreePart.purs
+++ src/ComposingCofree/ComposingCofreePart.purs
@@ -17,92 +17,92 @@ composingCofreePart = do
   = Pure a 
   | Free f (Free f a)
 
-  derive instance functorFree :: Functor (Free f)
+derive instance functorFree :: Functor (Free f)
 
-  instance bindFree :: Bind (Free f) where
-  bind (Pure a) f = f a
-  bind (Free m) f = (Free (f <$> m))
+instance bindFree :: Bind (Free f) where
+bind (Pure a) f = f a
+bind (Free m) f = (Free (f <$> m))
 
-  instance applyFree :: Apply (Free f) where
-  apply = ap
+instance applyFree :: Apply (Free f) where
+apply = ap
 
-  instance applicativeFree :: Applicative (Free f) where
-  pure a = Pure a
+instance applicativeFree :: Applicative (Free f) where
+pure a = Pure a
 
-  instance monadFree :: Monad (Free f)"""
+instance monadFree :: Monad (Free f)"""
   section $ do
     h1 $ text "Comonad"
     h3 $ text "definition"
     pre $ code ! className "language-haskell" $ text """-- | Associativity: `extend f <<< extend g = extend (f <<< extend g)`
-  class Functor w <= Extend w where"""
+class Functor w <= Extend w where"""
 
     pre $ code ! className "language-haskell" $ text """-- | Forwards co-Kleisli composition.
-  composeCoKleisli
+composeCoKleisli
   :: forall b a w c
-  . Extend w
+   . Extend w
   => (w a -> b)
   -> (w b -> c)
   -> w a -> c
-  composeCoKleisli f g w = g (f <<= w)
+composeCoKleisli f g w = g (f <<= w)
 
-  -- | Duplicate a comonadic context.
-  duplicate
+-- | Duplicate a comonadic context.
+duplicate
   :: forall a w
-  . Extend w
+   . Extend w
   => w a -> w (w a)
-  duplicate = extend id"""
+duplicate = extend id"""
     pre $ code ! className "language-haskell" $ text """-- | Left Identity: `extract <<= xs = xs`
-  -- | Right Identity: `extract (f <<= xs) = f xs`
-  class Extend w <= Comonad w where
+-- | Right Identity: `extract (f <<= xs) = f xs`
+class Extend w <= Comonad w where
   extract :: forall a. w a -> a"""
   section $ do
     h1 $ text "Stream Comonad"
     pre $ code ! className "language-haskell" $ text """data Stream a = Cons a (Lazy (Stream a))
 
-  derive instance functorStream :: Functor Stream
+derive instance functorStream :: Functor Stream
 
-  instance extendStream :: Extend Stream where
-  extend f s@(Cons a r) = Cons (f s) (extend f <$> r)
+instance extendStream :: Extend Stream where
+extend f s@(Cons a r) = Cons (f s) (extend f <$> r)
 
-  instance comonadStream :: Comonad Stream where
-  extract (Cons a _) = a
+instance comonadStream :: Comonad Stream where
+extract (Cons a _) = a
 
-  smooth :: Int -> Stream Number -> Stream Number
-  smooth n s = extend (avN n) s
+smooth :: Int -> Stream Number -> Stream Number
+smooth n s = extend (avN n) s
   where
-  sumN :: Int -> Stream Number -> Number
-  sumN 0 _ = 0.0
-  sumN n (Cons a r) = a + (sumN (n - 1) (force r))
+    sumN :: Int -> Stream Number -> Number
+    sumN 0 _ = 0.0
+    sumN n (Cons a r) = a + (sumN (n - 1) (force r))
 
-  avN :: Int -> Stream Number -> Number
-  avN n s = (sumN n s) / (toNumber n)"""
+    avN :: Int -> Stream Number -> Number
+    avN n s = (sumN n s) / (toNumber n)"""
   section $ do
     h1 $ text "Cofree comonad"
     text "Given a functor `f` we can build a cofree comonad"
     pre $ code ! className "language-haskell" $ text """
-  data Cofree f a = Cofree a (Lazy (f (Cofree f a)))
+data Cofree f a = Cofree a (Lazy (f (Cofree f a)))
 
-  mkCofree :: forall f a. a -> f (Cofree f a) -> Cofree f a
-  mkCofree a t = Cofree a (defer \_ -> t)
+mkCofree :: forall f a. a -> f (Cofree f a) -> Cofree f a
+mkCofree a t = Cofree a (defer \_ -> t)
 
-  infixr 5 mkCofree as :<
+infixr 5 mkCofree as :<
 
-  -- | Returns the label for a tree.
-  head :: forall f a. Cofree f a -> a
-  head (Cofree h _) = h
+-- | Returns the label for a tree.
+head :: forall f a. Cofree f a -> a
+head (Cofree h _) = h
 
-  instance functorCofree :: Functor f => Functor (Cofree f) where
-  map f = loop where
+instance functorCofree :: Functor f => Functor (Cofree f) where
+map f = loop where
   loop fa@(Cofree head _tail)
-  = Cofree (f head) ((map loop) <$> _tail)
+    = Cofree (f head) ((map loop) <$> _tail)
 
-  instance extendCofree :: Functor f => Extend (Cofree f) where
-  extend f = loop
+instance extendCofree :: Functor f => Extend (Cofree f) where
+extend f = loop
   where
-  loop fa@(Cofree _ _tail)
-  = Cofree (f fa) ((map loop) <$> _tail)
+    loop fa@(Cofree _ _tail)
+      = Cofree (f fa) ((map loop) <$> _tail)
 
-  instance comonadCofree :: Functor f => Comonad (Cofree f) where
+instance comonadCofree :: Functor f => Comonad (Cofree f) where
   extract = head"""
   section $ do
     h1 $ text "Composing interpreters"
@@ -115,7 +115,7 @@ composingCofreePart = do
   -> (s -> f s)
   -> s
   -> Cofree f a
-  unfoldCofree e n s =
+unfoldCofree e n s =
   Cofree (e s) (defer \_ -> unfoldCofree e n <$> n s)"""
     pre $ code ! className "language-haskell" $ text """explore
   :: forall f g a b
@@ -125,40 +125,40 @@ composingCofreePart = do
   -> Free f (a -> b)
   -> Cofree g a
   -> b
-  explore pair m w =
+explore pair m w =
   case runState (runFreeM step m) w of
-  Tuple f cof -> f (extract cof)
+    Tuple f cof -> f (extract cof)
   where
-  step
-  :: f (Free f (a -> b))
-  -> State (Cofree g a) (Free f (a -> b))
-  step ff = state \cof -> pair (map Tuple ff) (tail cof)"""
+    step
+      :: f (Free f (a -> b))
+      -> State (Cofree g a) (Free f (a -> b))
+    step ff = state \cof -> pair (map Tuple ff) (tail cof)"""
   section $ do
     pre $ code ! className "language-haskell" $ text """newtype Coproduct f g a = Coproduct (Either (f a) (g a))
 
-  -- | Left injection
-  left :: forall f g a. f a -> Coproduct f g a
-  left fa = Coproduct (Left fa)
+-- | Left injection
+left :: forall f g a. f a -> Coproduct f g a
+left fa = Coproduct (Left fa)
 
-  -- | Right injection
-  right :: forall f g a. g a -> Coproduct f g a
-  right ga = Coproduct (Right ga)"""
+-- | Right injection
+right :: forall f g a. g a -> Coproduct f g a
+right ga = Coproduct (Right ga)"""
     pre $ code ! className "language-haskell" $ text """newtype Product f g a = Product (Tuple (f a) (g a))
 
-  -- | Create a product.
-  product :: forall f g a. f a -> g a -> Product f g a
-  product fa ga = Product (Tuple fa ga)"""
+-- | Create a product.
+product :: forall f g a. f a -> g a -> Product f g a
+product fa ga = Product (Tuple fa ga)"""
   section $ do
     pre $ code ! className "language-haskell" $ text """-- | Returns the label for a tree.
-  head :: forall f a. Cofree f a -> a
-  head (Cofree h _) = h
+head :: forall f a. Cofree f a -> a
+head (Cofree h _) = h
 
-  -- | Returns the "subtrees" of a tree.
-  tail :: forall f a. Cofree f a -> f (Cofree f a)
-  tail (Cofree _ t) = force t
+-- | Returns the "subtrees" of a tree.
+tail :: forall f a. Cofree f a -> f (Cofree f a)
+tail (Cofree _ t) = force t
 
-  mkCofree :: forall f a. a -> f (Cofree f a) -> Cofree f a
-  mkCofree a t = Cofree a (defer \_ -> t)"""
+mkCofree :: forall f a. a -> f (Cofree f a) -> Cofree f a
+mkCofree a t = Cofree a (defer \_ -> t)"""
   section $ do
     h1 $ text "Composing Streams"
     p ! className "center" $ do
